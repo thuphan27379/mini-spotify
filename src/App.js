aa3d2802ecd9f500e674c9ffa1f8ffb56bdc6af2
thuphan27379 <131003628+thuphan27379@users.noreply.github.com>
2023-09-09 21:53:06 +0700

    update 1st

--- src/App.js
+++ src/App.js
@@ -1,66 +1,19 @@
-import { useContext } from "react";
-import { MusicPlayerContext } from "../contexts/MusicPlayerContext";
+import "./App.css";
+import { MusicPlayerProvider } from "./contexts/MusicPlayerContext";
+import Header from "./components/Header";
+import TrackList from "./components/TrackList";
+import Controller from "./components/Controller";
 
-const useMusicPlayer = () => {
-  const { state, setState } = useContext(MusicPlayerContext);
-
-  // Play a specific track
-  function playTrack(index) {
-    if (index === state.currentTrackIndex) {
-      togglePlay();
-    } else {
-      state.audioPlayer.pause();
-
-      const audio = new Audio(state.tracks[index].file);
-      audio.loop = true;
-      state.audioPlayer = audio;
-      state.audioPlayer.play();
-
-      setState((state) => ({
-        ...state,
-        currentTrackIndex: index,
-        isPlaying: true,
-      }));
-    }
-  }
-
-  // Toggle play or pause
-  function togglePlay() {
-    if (state.isPlaying) {
-      state.audioPlayer.pause();
-    } else {
-      state.audioPlayer.play();
-    }
-    setState((state) => ({ ...state, isPlaying: !state.isPlaying }));
-  }
-
-  // Play the previous track in the tracks array
-  function playPreviousTrack() {
-    const newIndex =
-      (((state.currentTrackIndex + -1) % state.tracks.length) +
-        state.tracks.length) %
-      state.tracks.length;
-    playTrack(newIndex);
-  }
-
-  // Play the next track in the tracks array
-  function playNextTrack() {
-    const newIndex = (state.currentTrackIndex + 1) % state.tracks.length;
-    playTrack(newIndex);
-  }
-
-  return {
-    playTrack,
-    togglePlay,
-    currentTrackName:
-      state.currentTrackIndex !== null &&
-      state.tracks[state.currentTrackIndex].name,
-    trackList: state.tracks,
-    isPlaying: state.isPlaying,
-    currentTrackIndex: state.currentTrackIndex,
-    playPreviousTrack,
-    playNextTrack,
-  };
+const App = () => {
+  return (
+    <MusicPlayerProvider>
+      <div className="container">
+        <Header />
+        <TrackList />
+        <Controller />
+      </div>
+    </MusicPlayerProvider>
+  );
 };
 
-export default useMusicPlayer;
+export default App;
--- src/contexts/MusicPlayerContext.js
+++ src/contexts/MusicPlayerContext.js
@@ -1,7 +1,7 @@
 import React, { useState, createContext } from "react";
-import Track1 from "../mp3/track_1.mp3";
-import Track2 from "../mp3/track_2.mp3";
-import Track3 from "../mp3/track_3.mp3";
+import Track1 from "../mp3/track1.mp3";
+import Track2 from "../mp3/track2.mp3";
+import Track3 from "../mp3/track3.mp3";
 
 const MusicPlayerContext = createContext();
 
